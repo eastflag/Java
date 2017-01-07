b3fbf8e03c01cc722b43786cfa716dc121683bf3
eastflag <eastflag@gmail.com>
2017-01-08 03:43:30 +0900

    람다식 예제 추가

--- src/main/java/$15_stream/$2_kind/FromIntRangeExample.java
+++ src/main/java/$15_stream/$2_kind/FromIntRangeExample.java
@@ -4,10 +4,16 @@ import java.util.stream.IntStream;
 
 public class FromIntRangeExample {
 	public static int sum;
-	
+
 	public static void main(String[] args) {
 		IntStream stream = IntStream.rangeClosed(1,  100);
 		stream.forEach(a -> sum += a);
-		System.out.println("����: " + sum);
+		System.out.println("총합: " + sum);
+
+		sum =0;
+		//range: first <= x < second
+		IntStream stream2 = IntStream.range(1, 100);
+		stream2.forEach(a -> sum += a);
+		System.out.println("총합2: " + sum);
 	}
 }
